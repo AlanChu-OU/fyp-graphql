f0281128daef4c66826caac288851b8bf1f37924
AlanChu-OU <g1186214@study.ouhk.edu.hk>
2020-05-23 22:38:53 +0800

    coach student model fix

--- models/coach/coachrequest.js
+++ models/coach/coachrequest.js
@@ -3,15 +3,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const coachrequestSchema = new Schema({
-    coachId: 
+    coach: 
     {
         type: Schema.Types.ObjectId,
-        ref: 'User'
+        ref: 'Coach'
     },
     userId:
     {
         type: Schema.Types.ObjectId,
-        ref: 'User'
+        ref: 'Student'
     },
     message:{
         type: String
